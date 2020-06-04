cdd8b448c0b1df23cadd4f82d881707a8c2c4836
erey <erey1234122@gmail.com>
2020-06-04 07:09:49 -0700

    Adding traversable extensions.
    Adding traversable extensions.

---
+++ src/LeanSharp/Extensions/TraversableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeanSharp.Extensions
+{
+    public static class TraversableExtensions
+    {
+        public static Result<T, string> MapToStringFailure<T>(this Result<T, Exception> result) 
+        => result.Either(
+                r => Result<T, string>.Succeeded(r.Success),
+                e => Result<T, string>.Failed(e.Failure.Message));
+        public static AsyncLazyPipeline<Result<TSuccess, TFailure>> ToPipeline<TSuccess, TFailure>(
+            this Result<TSuccess, TFailure> result)
+        => result.IsSuccess
+            ? CreatePipeLine.Return(Result<TSuccess, TFailure>.Succeeded(result.Success))
+            : CreatePipeLine.Return(Result<TSuccess, TFailure>.Failed(result.Failure));
+
+        public static AsyncLazyPipeline<Result<TSuccess, TFailure>> FlipMerge<TSuccess, TFailure>(
+            this Result<AsyncLazyPipeline<Result<TSuccess, TFailure>>, TFailure> result)
+        => result.EitherFold(r => r.Success, e => CreatePipeLine.Return(Result<TSuccess, TFailure>.Failed(e.Failure)));
+
+        public static AsyncLazyPipeline<Result<TSuccess, TFailure>> FlipMerge<TSuccess, TFailure>(
+            this AsyncLazyPipeline<Result<AsyncLazyPipeline<TSuccess>, TFailure>> pipeLine)
+        => pipeLine
+            .SelectMany(result =>
+                result.EitherFold(
+                    r => r.Success.Select(Result<TSuccess, TFailure>.Succeeded), 
+                    error => CreatePipeLine.Return(Result<TSuccess, TFailure>.Failed(error.Failure))));
+    }
+}
