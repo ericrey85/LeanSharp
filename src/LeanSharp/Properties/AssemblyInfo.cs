facc1993b56d44fed9427266e09208e3e790f28d
erey <erey1234122@gmail.com>
2020-10-03 11:56:53 -0400

    Updating package version after changes.

--- src/LeanSharp/Properties/AssemblyInfo.cs
+++ src/LeanSharp/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("LeanSharp")]
 [assembly: AssemblyProduct("LeanSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3540E903-A975-4306-B37B-6600B79DD8FE")]
-[assembly: AssemblyVersion("1.0.8")]
+[assembly: AssemblyVersion("1.0.9")]
