6e9211256874c58c95d553ed93ba8d34ef351e07
erey <erey1234122@gmail.com>
2020-10-05 14:24:53 -0400

    Adding new safe and declarative methods to IEnumerable<T>

--- src/LeanSharp/Extensions/SafeEnumerableExtensions.cs
+++ src/LeanSharp/Extensions/SafeEnumerableExtensions.cs
@@ -17,5 +17,45 @@ namespace LeanSharp.Extensions
 
         public static int SafeCount<T>(this IEnumerable<T> @this, Func<T, bool> predicate)
         => @this?.Count(predicate) ?? 0;
+
+        public static TDestination IfSafeAnyMap<TSource, TDestination>(
+            this IEnumerable<TSource> @this,
+            Func<IEnumerable<TSource>, TDestination> fn,
+            TDestination defaultValue)
+        => @this?.Any() == true ? fn(@this) : defaultValue;
+
+        public static TDestination IfSafeAnyMap<TSource, TDestination>(
+            this IEnumerable<TSource> @this,
+            Func<TSource, bool> predicate,
+            Func<IEnumerable<TSource>, TDestination> mapFiltered,
+            TDestination defaultValue)
+        {
+            var filteredElements = @this?.Where(predicate) ?? new List<TSource>();
+
+            return filteredElements.Any() ? mapFiltered(filteredElements) : defaultValue;
+        }
+
+        public static void IfSafeAnyDo<TSource>(
+            this IEnumerable<TSource> @this,
+            Action<IEnumerable<TSource>> fn)
+        {
+            if (@this?.Any() == true)
+            {
+                fn(@this);
+            }
+        }
+
+        public static void IfSafeAnyDo<TSource>(
+            this IEnumerable<TSource> @this,
+            Func<TSource, bool> predicate,
+            Action<IEnumerable<TSource>> fn)
+        {
+            var filteredElements = @this?.Where(predicate) ?? new List<TSource>();
+
+            if (filteredElements.Any())
+            {
+                fn(filteredElements);
+            }
+        }
     }
 }
--- tests/LeanSharp.Tests/SafeEnumerableExtensionsTests.cs
+++ tests/LeanSharp.Tests/SafeEnumerableExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using LeanSharp.Extensions;
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace LeanSharp.Tests
@@ -41,5 +42,59 @@ namespace LeanSharp.Tests
             Assert.Equal(3, numbers.SafeCount());
             Assert.Equal(2, numbers.SafeCount(number => number > 1));
         }
+
+        [Fact]
+        public void IfSafeAnyMap_ExecutesMappingFunctionIfCollectionIsNotNull()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            var sum1 = numbers.IfSafeAnyMap(nums => nums.Aggregate(Add), -1);
+            var sum2 = numbers.IfSafeAnyMap(n => n > 1, nums => nums.Aggregate(Add), -1);
+
+            Assert.Equal(6, sum1);
+            Assert.Equal(5, sum2);
+        }
+
+        [Fact]
+        public void IfSafeAnyMap_ReturnsDefaultValueIfCollectionIsNull()
+        {
+            List<int> numbers = null;
+
+            var sum1 = numbers.IfSafeAnyMap(nums => nums.Aggregate(Add), -1);
+            var sum2 = numbers.IfSafeAnyMap(n => n > 1, nums => nums.Aggregate(Add), -1);
+
+            Assert.Equal(-1, sum1);
+            Assert.Equal(-1, sum2);
+        }
+
+        [Fact]
+        public void IfSafeAnyDo_ExecutesDelegateIfCollectionIsNotNull()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+            var sum1 = 0;
+            var sum2 = 0;
+
+            numbers.IfSafeAnyDo(nums => { sum1 = nums.Aggregate(Add); });
+            numbers.IfSafeAnyDo(n => n > 1, nums => { sum2 = nums.Aggregate(Add); });
+
+            Assert.Equal(6, sum1);
+            Assert.Equal(5, sum2);
+        }
+
+        [Fact]
+        public void IfSafeAnyDo_DoNotExecuteDelegateIfCollectionIsNull()
+        {
+            List<int> numbers = null;
+            var sum1 = 0;
+            var sum2 = 0;
+
+            numbers.IfSafeAnyDo(nums => { sum1 = nums.Aggregate(Add); });
+            numbers.IfSafeAnyDo(n => n > 1, nums => { sum2 = nums.Aggregate(Add); });
+
+            Assert.Equal(0, sum1);
+            Assert.Equal(0, sum2);
+        }
+
+        private int Add(int number1, int number2) => number1 + number2;
     }
 }
