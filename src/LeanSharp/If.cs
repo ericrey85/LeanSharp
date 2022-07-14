2189e1367926bfc05654db6426632fde35c1f741
ericrey85 <ericrey85@gmail.com>
2022-07-14 16:12:41 -0400

    Adding simple declarative if/then construct.

---
+++ src/LeanSharp/If.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace LeanSharp
+{
+    public abstract class If
+    {
+        public static If True(Func<bool> predicate)
+        {
+            if (predicate())
+            {
+                return new ExpressionExecuter();
+            }
+            return new ExpressionDisregarder();
+        }
+
+        public static If False(Func<bool> predicate)
+        {
+            if (!predicate())
+            {
+                return new ExpressionExecuter();
+            }
+            return new ExpressionDisregarder();
+        }
+
+        public abstract If Then(Action action);
+
+        private class ExpressionExecuter : If
+        {
+            public override If Then(Action action)
+            {
+                action();
+                return this;
+            }
+        }
+
+        private class ExpressionDisregarder : If
+        {
+            public override If Then(Action action)
+            {
+                return this;
+            }
+        }
+    }
+}
+
---
+++ tests/LeanSharp.Tests/IfTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using LeanSharp.Tests.Extensions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LeanSharp.Tests
+{
+    public class IfTests
+    {
+        private ITestOutputHelper Output { get; }
+
+        public IfTests(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        [Fact]
+        public void IfTrueThen_ExecutesActionIfConditionalIsTrue()
+        {
+            void Action() => Output.WriteLine("1");
+
+            If.True(() => 3 == 3)
+                .Then(Action)
+                .Then(Action);
+
+            Assert.Equal("11", Output.GetOutputAsString());
+        }
+
+        [Fact]
+        public void IfTrueThen_DoesNotExecuteActionIfConditionalIsFalse()
+        {
+            void Action() => Output.WriteLine("1");
+
+            If.True(() => 2 == 3).Then(Action);
+
+            Assert.NotEqual("1", Output.GetOutputAsString());
+        }
+
+        [Fact]
+        public void IfFalseThen_ExecutesActionIfConditionalIsFalse()
+        {
+            void Action() => Output.WriteLine("1");
+
+            If.False(() => 2 == 3)
+                .Then(Action)
+                .Then(Action);
+
+            Assert.Equal("11", Output.GetOutputAsString());
+        }
+
+        [Fact]
+        public void IfFalseThen_DoesNotExecuteActionIfConditionalIsTrue()
+        {
+            void Action() => Output.WriteLine("1");
+
+            If.False(() => 3 == 3).Then(Action);
+
+            Assert.NotEqual("1", Output.GetOutputAsString());
+        }
+    }
+}
+
